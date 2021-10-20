0b046eb25f764b011caf7e1174ad0dd1ec0a5376
Isaac Kimura <isaac.kimura@iprospect.com>
2021-10-20 21:45:12 +0300

    Created the main section for the site & added the home page, done with the design of the home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import styled from "styled-components";
 import Sidebar from "./Components/Sidebar";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <Container>
       <Sidebar />
+      <MainContentStyled>
+        <Lines>
+          <Line></Line>
+          <Line></Line>
+          <Line></Line>
+          <Line></Line>
+        </Lines>
+        <HomePage />
+      </MainContentStyled>
     </Container>
   );
 }
 
-export default App;
-
 const Container = styled.div``;
+
+const MainContentStyled = styled.main`
+  position: relative;
+  margin-left: 16.3rem;
+  min-height: 100vh;
+`;
+
+const Lines = styled.div`
+  position: absolute;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Line = styled.div`
+  width: 0.1rem;
+  min-height: 100vh;
+  background-color: var(--border-color);
+`;
+
+export default App;
