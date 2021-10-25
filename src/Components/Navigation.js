f588c83717881824607f1cc4949ccb95ad1afa67
Isaac Kimura <Isaac.Kimura@iprospect.com>
2021-10-25 08:14:53 +0300

    Renamed the Avatar image

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import avatar from "../img/Isaac Karanja Kimura.jpg";
+import avatar from "../img/Isaac-Karanja-Kimura.jpg";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
