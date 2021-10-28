785fd73bb3c3caec4cfa24033212bddf8bd8aa57
Isaac Kimura <isaac.kimura@iprospect.com>
2021-10-29 02:23:44 +0300

    Added the blogs dammy data

---
+++ src/data/blogs.js
@@ -0,0 +1,55 @@
+import blog1 from "../img/seo-expert-soutebanseo-1.jpg";
+import blog2 from "../img/on-page-off-page-seo.jpg";
+import blog3 from "../img/seo-expert-rochil-seo-1.jpg";
+import blog4 from "../img/seo.jpg";
+import blog5 from "../img/digital-marketing-agency-pittsburg-ks-pitsrg-seo-1.jpg";
+
+const blogs = [
+  {
+    id: 1,
+    title: "Difference Between On-Page & Off-Page SEO",
+    date: "26",
+    month: "October",
+    year: "2021",
+    image: blog2,
+    link:"https://www.google.com/"
+  },
+  {
+    id: 2,
+    title: "What Is SEO, Why Is Important To Your Business",
+    date: "26",
+    month: "October",
+    year: "2021",
+    image: blog1,
+    link:"https://www.google.com/"
+  },
+  {
+    id: 3,
+    title: "Using SEO Effectively For Higher ROI",
+    date: "29",
+    month: "October",
+    year: "2021",
+    image: blog4,
+    link:"https://www.google.com/"
+  },
+  {
+    id: 4,
+    title: "Keyword Research: Best Practices",
+    date: "09",
+    month: "November",
+    year: "2021",
+    image: blog3,
+    link:"https://www.google.com/"
+  },
+  {
+    id: 5,
+    title: "React Js: Become A MERN Stack Developer",
+    date: "16",
+    month: "November",
+    year: "2021",
+    image: blog5,
+    link:"https://www.google.com/"
+  },
+];
+
+export default blogs;
